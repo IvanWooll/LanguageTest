d2aa45dc2f1849e5088d824a3bb7952e9ce14d25
ivanwooll <ivan@codegiants.com>
2020-06-18 11:43:31 +0100

    Version changes the language via device settings

--- app/src/main/java/com/codegiants/languagetest/Element.kt
+++
@@ -1,7 +0,0 @@
-package com.codegiants.languagetest
-
-data class Element(
-    val noteContent: String,
-    val noteCurrentTime: Long,
-    val noteTitle: String
-)
--- app/src/main/java/com/codegiants/languagetest/MainActivity.kt
+++ app/src/main/java/com/codegiants/languagetest/MainActivity.kt
@@ -2,14 +2,10 @@ package com.codegiants.languagetest
 
 import android.content.ComponentName
 import android.content.Intent
-import android.content.res.Resources
 import android.os.Build
 import android.os.Bundle
-import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.os.ConfigurationCompat
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,42 +15,7 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
-        val appLanguage = Locale.getDefault()
-        println("app language=$appLanguage")
-
-        val locales = ConfigurationCompat.getLocales(Resources.getSystem().configuration)
-
-        println("device language=${locales[0]}")
-
         button.setOnClickListener {
-            PopupMenu(this, button).apply {
-                menuInflater.inflate(R.menu.main, menu)
-                setOnMenuItemClickListener { item ->
-                    when (item.itemId) {
-                        R.id.en -> {
-                            App.changeLanguage(
-                                "en",
-                                this@MainActivity,
-                                Intent(this@MainActivity, MainActivity::class.java)
-                            )
-                            true
-                        }
-                        R.id.fr -> {
-                            App.changeLanguage(
-                                "fr",
-                                this@MainActivity,
-                                Intent(this@MainActivity, MainActivity::class.java)
-                            )
-
-                            true
-                        }
-                        else -> false
-                    }
-                }
-            }.show()
-        }
-
-        buttonOpenActivity.setOnClickListener {
             val intent = Intent()
 
             val cls = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -63,10 +24,7 @@ class MainActivity : AppCompatActivity() {
                 "com.android.settings.Settings\$InputMethodAndLanguageSettingsActivity"
             }
 
-            intent.component = ComponentName(
-                "com.android.settings",
-                cls
-            )
+            intent.component = ComponentName("com.android.settings", cls)
 
             startActivity(intent)
         }
